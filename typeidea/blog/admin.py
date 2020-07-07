98d9a26ad4d5affd71be9400e327b51c911ddecf
gg13021123732.163.com <gg13021123732.163.com>
2020-07-07 17:06:21 +0800

    后台初步提交

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -6,14 +6,36 @@ from django.utils.html import format_html
 from .models import Post, Category, Tag
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """  自定义过滤器只展示当前用户分类  """
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'create_time')
     fields = ('name', 'status', 'is_nav')
 
     def save_model(self, request, obj, form, change):
+        print(obj)
         obj.owner = request.user
         return super().save_model(request, obj, form, change)
+
+    def post_count(self, obj):
+        counter = obj.post_set.count()
+        print(counter)
+        return counter
+
+    post_count.short_description = '文章数量'
 
 
 @admin.register(Tag)
@@ -30,11 +52,11 @@ class TagAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'title', 'category', 'status',
-        'create_time', 'operator'
+        'create_time', 'owner', 'operator'
     ]
     list_display_links = []
 
-    list_filter = ['category']
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
 
     actions_on_bottom = True
--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django.urls import reverse
 # Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('target', 'nickname', 'content', 'website', 'create_time')
